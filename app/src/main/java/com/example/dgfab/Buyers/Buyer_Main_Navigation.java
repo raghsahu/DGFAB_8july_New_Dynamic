1668ecd904268fec772649c1f95f47b26246643f
NothingbutPro <paragsharma611@gmail.com>
2019-07-09 17:43:34 +0530

    paragwaaaaaa

---
+++ app/src/main/java/com/example/dgfab/Buyers/Buyer_Main_Navigation.java
@@ -0,0 +1,27 @@
+package com.example.dgfab.Buyers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.dgfab.Activity.Buyer_guest_login.Use_reason_Activity;
+import com.example.dgfab.LoginandReg.ManuLoginActivity;
+import com.example.dgfab.R;
+public class Buyer_Main_Navigation extends AppCompatActivity {
+    TextView logorreg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer__main__navigation);
+        logorreg =findViewById(R.id.logorreg);
+        logorreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Buyer_Main_Navigation.this , ManuLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
