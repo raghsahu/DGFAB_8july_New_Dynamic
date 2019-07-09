4dc1964ad83a967ad1da6085b4a88d273602643f
Raghvendra Sahu <raghvendra.19934@gmail.com>
2019-07-09 12:26:16 +0530

    by rrrr dgfab

--- app/src/main/java/com/example/dgfab/Activity/LoginActivity.java
+++ app/src/main/java/com/example/dgfab/Activity/LoginActivity.java
@@ -2,14 +2,33 @@ package com.example.dgfab.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.dgfab.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView tv_optcode;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tv_optcode=findViewById(R.id.tv_optcode);
+
+        tv_optcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+            }
+        });
+
+
     }
 }
